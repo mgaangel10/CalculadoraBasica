62e47019254af73f90bfb27a0ebb5b76ab5b6e0f
miguel angel <mg.aangel10@gmail.com>
2023-04-17 09:21:13 +0200

    proyecto

---
+++ src/calculadora/Principal.java
@@ -0,0 +1,28 @@
+package calculadora;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int op=0;
+		
+		System.out.println("Calculadora Básica");
+		do {
+			System.out.println("1---Sumar");
+		System.out.println("2---Restar");
+		System.out.println("3---Multiplicar");
+		System.out.println("4---Division resto");
+		switch (op) {
+		case 1:
+			
+			break;
+
+		default:
+			break;
+		}
+		} while (op!=0);
+		
+		
+	}
+
+}
